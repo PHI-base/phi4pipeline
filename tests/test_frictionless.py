b715d3b18706a54b489d28c2337a4da094752a9a
James Seager <james.seager@rothamsted.ac.uk>
2024-06-06 16:00:33 +0100

    Extract expected datapackage to fixture

--- tests/test_frictionless.py
+++ tests/test_frictionless.py
@@ -14,23 +14,26 @@ DATA_DIR = importlib.resources.files('phi4pipeline') / 'metadata'
 TEST_DATA_DIR = Path(__file__).parent / 'data'
 
 
-def test_load_formatted_datapackage():
+@pytest.fixture
+def datapackage_json():
+    path = TEST_DATA_DIR / 'datapackage_expected.json'
+    with open(path) as file:
+        datapackage = json.load(file)
+    return datapackage
+
+
+def test_load_formatted_datapackage(datapackage_json):
     template_path = DATA_DIR / 'datapackage_template.json'
-
-    expected_path = TEST_DATA_DIR / 'datapackage_expected.json'
-    with open(expected_path) as expected_file:
-        expected = json.load(expected_file)
-
     format_args = {
         'version': '4.12',
-        'doi': 'https://doi.org/10.5281/zenodo.5356871',
+        'doi': f'https://doi.org/10.5281/zenodo.5356871',
         'phibase_hash': '2f3c683d41d46de6a0a20f56ba29113ab1449936',
         'phibase_bytes': '8998',
         'fasta_hash': '1a65c4809dfa91ea35ae0bd5b3f5c6221e0eab35',
         'fasta_bytes': '5632',
     }
-
     actual = load_formatted_datapackage(template_path, format_args)
+    expected = datapackage_json
     assert actual == expected
 
 
